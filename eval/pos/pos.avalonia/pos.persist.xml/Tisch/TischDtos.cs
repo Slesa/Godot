218862039a1d303798f57e47b76e7cd67b23dae2
Jörg Preiß <joerg.preiss@slesa.de>
2019-03-24 00:39:05 +0100

    XML Format verbessert

--- eval/pos/pos.avalonia/pos.persist.xml/Tisch/TischDtos.cs
+++ eval/pos/pos.avalonia/pos.persist.xml/Tisch/TischDtos.cs
@@ -10,7 +10,6 @@ namespace pos.persist.xml.Tisch
         public DateTime OccurredOn { get; set; }
     }
 
-    [XmlRoot(ElementName = "Bestellung")]
     public class ArtikelBestelltDto : TischEintragDto
     {
         public uint Anzahl { get; set; }
@@ -18,7 +17,6 @@ namespace pos.persist.xml.Tisch
         public decimal Preis { get; set; }
     }
 
-    [XmlRoot(ElementName = "Storno")]
     public class ArtikelStorniertDto : TischEintragDto
     {
         public uint Anzahl { get; set; }
@@ -34,7 +32,8 @@ namespace pos.persist.xml.Tisch
     public class TischDto
     {
         List<TischEintragDto> _tischInhalt;
-        [XmlArrayItem("TischInhalt")]
+        [XmlArray("TischInhalt")]
+        [XmlArrayItem("Eintrag")]
         public List<TischEintragDto> TischInhalt
         {
             get => _tischInhalt ?? (_tischInhalt = new List<TischEintragDto>());

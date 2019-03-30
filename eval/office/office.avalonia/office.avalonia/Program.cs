c1e308b2de75cff009ed699f999fa410fc566d2e
Jörg Preiß <joerg.preiss@slesa.de>
2019-03-31 00:24:40 +0100

    Adding eval of POS office

---
+++ eval/office/office.avalonia/office.avalonia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Logging.Serilog;
+using office.avalonia.ViewModels;
+using office.avalonia.Views;
+
+namespace office.avalonia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
+    }
+}

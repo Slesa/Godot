d09514d7ed1ad42058f207b9a879f9e81981f932
Jörg Preiß <joerg.preiss@slesa.de>
2019-04-18 22:26:34 +0200

    Umfrage erzeugen

---
+++ eval/gui/heroes/heroes.react/src/client/components/VoteSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function VoteSummary({vote, onActivate}) {
+  const totalVotes = vote.choices.reduce((prev, curr) => prev+curr.count, 0);
+
+  return (
+    <div onClick={onActivate} className="Row VotesRow Selectable">
+      <h1 className="Title">{vote.title}
+        <div className="Badge">{totalVotes}</div>
+      </h1>
+      <p className="Emphasis">{vote.description}</p>
+    </div>
+  );
+}
+VoteSummary.propTypes = {
+  vote:       React.PropTypes.object.isRequired,
+  onActivate: React.PropTypes.func.isRequired
+};
--- eval/gui/heroes/heroes.react/src/client/main.js
+++ eval/gui/heroes/heroes.react/src/client/main.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Layout from './components/Layout'
-import VotingComponent from './components/VotingComponent'
+import VoteController from './components/VoteController'
 
-const vote = {
-  title: 'How is your day?',
-  description: 'Tell me: how has your day been so far?',
-  choices: [
-    { id: 'choice_1', title: 'Good', count: 7 },
-    { id: 'choice_2', title: 'Bad', count: 12 },
-    { id: 'choice_3', title: 'Not sure yet', count: 1 }
-  ]
-};
+const allVotes = [
+  {
+    id:          'vote_1',
+    title:       'How is your day?',
+    description: 'Tell me: how has your day been so far?',
+    choices: [
+      { id: 'choice_1', title: 'Good', count: 7 },
+      { id: 'choice_2', title: 'Bad', count: 12 },
+      { id: 'choice_3', title: 'Not sure yet', count: 1 }
+    ]
+  },
+  {
+    id:          'vote_2',
+    title:       'Programming languages',
+    description: 'What is your preferred language?',
+    choices: [
+      {id: 'choice_1', title: 'JavaScript', count: 5},
+      {id: 'choice_2', title: 'Java', count: 9},
+      {id: 'choice_3', title: 'Plain english', count: 17}
+    ]
+  }
+];
+
+const mainComponent = <VoteController allVotes={allVotes}/>;
 
 ReactDOM.render(
-  <Layout>
-    <VotingComponent vote={vote} />
-  </Layout>,
+  <Layout>{mainComponent}</Layout>,
   document.getElementById('voteAppMountPoint')
 );
